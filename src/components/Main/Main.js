61aba62401c0f13f1c798ab14f090eb0dbdae905
Moon <nastasya_ivanova_2015@mail.ru>
2020-12-12 18:02:42 +0400

    add section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './main.module.css'; 
 import ThemeContext from "../../utils/ThemeContext";
+import { gifts } from "./gifts";
 
 const blocks = ["С вашим текстом", "Для него", "Для неё"];
 
@@ -17,11 +18,11 @@ const Main = () => {
                 ))}
             </ul>
             <div className={styles.images_block}>
-                {gifts.map((gift) => (
+                {gifts.filter(gift => gift.section === activeBlock).map(filteredGift => (
                     <div>
-                        <div style={{overflow:"hidden"}}><img src={gift.giftImage} alt={gift.name} /></div>
-                        <p>{gift.name}</p>
-                        <h3>{gift.price}</h3>
+                        <div style={{overflow:"hidden"}}><img src={filteredGift.giftImage} alt={filteredGift.name} /></div>
+                        <p>{filteredGift.name}</p>
+                        <h3>{filteredGift.price}</h3>
                     </div>
                 ))}
             </div>
@@ -30,21 +31,3 @@ const Main = () => {
 }
 
 export default Main;
-
-const gifts = [
-    {
-        name: "Кружка с вашим фото «Лучшей подруге»",
-        giftImage: require('../../img/cup.jpg').default,
-        price: "650 руб"
-    },
-    {
-        name: "Кружка с вашим именем «С 8 марта»",
-        giftImage: require('../../img/cup1.jpg').default,
-        price: "650 руб"
-    },
-    {
-        name: "Кружка с вашим именем «Хоть и не военный»",
-        giftImage: require('../../img/cup2.jpg').default,
-        price: "650 руб"
-    }
-];
